10ebb41a9db97537743c6af140f7fcfccdc97428
NTiger07 <falaleru@gmail.com>
2023-02-16 23:15:58 +0100

    fix

--- src/components/MoviesContainer.jsx
+++ src/components/MoviesContainer.jsx
@@ -5,18 +5,20 @@ import "./MoviesContainer.css";
 
 const MoviesContainer = (props) => {
   const movieselem = props.movies.map((movie) => {
-    return (
-      <Link to={`/movies/${movie.imdbID}`}>
-        <MoviesItem
-          key={movie.imdbID}
-          poster={movie.Poster}
-          title={movie.Title}
-          rating={movie.imdbRating}
-          year={movie.Year}
-          length={movie.Runtime}
-        />
-      </Link>
-    );
+    if (movie.Poster) {
+      return (
+        <Link to={`/movies/${movie.imdbID}`}>
+          <MoviesItem
+            key={movie.imdbID}
+            poster={movie.Poster}
+            title={movie.Title}
+            rating={movie.imdbRating}
+            year={movie.Year}
+            length={movie.Runtime}
+          />
+        </Link>
+      );
+    }
   });
 
   return <div className="movies_container">{movieselem}</div>;
